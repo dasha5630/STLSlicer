8ee22a52138d7bd645ea4320e93b85aa41596122
Dasha <dasha5630@yandex.ru>
2019-12-12 04:54:52 +0300

    open file and get it name

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,39 +1,59 @@
 package com.example.myapplication;
 
+import android.content.*;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 import android.app.Activity;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     final String LOG_TAG = "myLogs";
 
-    final String FILENAME = "file";
+    String FILENAME = "file";
+
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        Intent intent = getIntent();
+        String action = intent.getAction();
+
+        if (action.compareTo(Intent.ACTION_VIEW) == 0) {
+            String scheme = intent.getScheme();
+            ContentResolver resolver = getContentResolver();
+             Uri uri = intent.getData();
+                String name = uri.getLastPathSegment();
+
+                Log.v("tag", "File intent detected: " + action + " : " + intent.getDataString() + " : " + intent.getType() + " : " + name);
+                InputStream input = null;
+                try {
+                    input = resolver.openInputStream(uri);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                String importfilepath = name;
+                FILENAME = (String) name;
+                InputStreamToFile(input, importfilepath);
+        }
+    }
     public void onclick(View v) {
         switch (v.getId()) {
             case R.id.btnRead:
-                readFile();
+                if(FILENAME != "file")
+                    openText(v);
                 break;
         }
     }
@@ -52,6 +72,64 @@ public class MainActivity extends Activity {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private String getContentName(ContentResolver resolver, Uri uri){
+        Cursor cursor = resolver.query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        int nameIndex = cursor.getColumnIndex(MediaStore.MediaColumns.DISPLAY_NAME);
+        if (nameIndex >= 0) {
+            return cursor.getString(nameIndex);
+        } else {
+            return null;
+        }
+    }
+
+    private void InputStreamToFile(InputStream in, String file) {
+
+        try {
+            OutputStream out = new FileOutputStream(new File(file));
+
+            int size = 0;
+            byte[] buffer = new byte[1024];
+
+            while ((size = in.read(buffer)) != -1) {
+                out.write(buffer, 0, size);
+            }
+
+            out.close();
+        }
+        catch (Exception e) {
+            Log.e("MainActivity", "InputStreamToFile exception: " + e.getMessage());
+        }
+    }
+
+    public void openText(View view){
+
+        FileInputStream fin = null;
+        TextView textView = (TextView) findViewById(R.id.open_text);
+        try {
+            fin = openFileInput(FILENAME);
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            String text = new String (bytes);
+            textView.setText(text);
+        }
+        catch(IOException ex) {
+
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        finally{
+
+            try{
+                if(fin!=null)
+                    fin.close();
+            }
+            catch(IOException ex){
+
+                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
